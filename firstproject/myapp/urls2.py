e3e18b9cda851a59eccdebdda4a4fd4c480a6cf7
erikLopez77 <espinozalopezerik@gmail.com>
2026-03-11 10:08:52 -0600

    feat: messages, authenticate, login and protected views

--- firstproject/myapp/urls2.py
+++ firstproject/myapp/urls2.py
@@ -5,9 +5,9 @@ urlpatterns=[
     #views.index es la funcion index de views
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
-    path('login/', views.login, name='login'),
+    path('login/', views.auth_login, name='login'),
     path('addbook/', views.addbook, name='addbook'),
     path("getbook/<int:id>/", views.getbook,name="getbook"),
     path('books/<int:price>/', views.books, name='booksprice'),
-
+    path('myview/', views.myview, name="myview"),
 ]
--- firstproject/myapp/views.py
+++ firstproject/myapp/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from urllib import request
+
+from django.shortcuts import render,redirect
 # Create your views here.
 from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .models import col
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, "index.html")
@@ -10,21 +14,41 @@ def index(request):
 def about(request):
     return render(request, "about.html")
 
-def login(request):
-    if request.method == 'POST':
-        name = request.POST.get("username")
-        password = request.POST.get("password")
-        if name =="" or password =="":
-            messages.error(request, "required")
-        if len(request.POST.get('password'))<9:
-            messages.warning(request, "Weak Password")
-        if name in ['admin', 'manager', 'superuser']:
-            messages.error(request, "Username Not Available")
+def auth_login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username,password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login successful. Hello {}".format(user))
+            return redirect('home')
         else:
-            messages.success(request, "Login Successful.Welcome "+name)
-            return HttpResponse("success")
-    return render(request, "login.html")
+            messages.error(request, ("There Was An Error Logging In, Try Again..."))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
 
+def logout_user(request):
+    logout(request)
+    messages.info(request, "You Were Logged Out!")
+    return redirect('index')
+#we have to create UserCreationForm class
+""" 
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,
+            password=password)
+            login(request, user)
+            messages.success(request, ("Registration Successful!"))
+            return redirect('index')  
+    else:
+        return render(request, 'register.html', {}) """
 def addbook(request):
     if request.method=="POST":
         data = request.POST
@@ -54,3 +78,7 @@ def getbook(request, id):
     book = col.find_one({"id":id})
     return HttpResponse("<h2>Title: {} \t Author: {} \tPrice: {}</h2>".format(book['title'], book['author'],
     book['price']))
+
+@login_required(login_url="../login/")
+def myview(request):
+    return HttpResponse("This message will be displayed only if a user is logged in")
